d592c0ba321be0bc908211198e47d67d134cb070
root <osheen.masih@cactusglobal.com>
2020-09-08 13:09:56 +0530

    correction for empty file ..as empty file was being created but for size 1 byte

--- spot-commands.sh
+++ spot-commands.sh
@@ -57,36 +57,41 @@ else
 			fi
 		else 
 			echo "Listing the experiments"
+			echo > outfile
+
 			aws lambda invoke --function-name "nv-manage-spot-edit-score-dev" --region us-east-1 --payload '{"action":"'$action'"}' listfile 
+			#cat listfile
 			a=`sed -n "/Training/p" listfile`
 			echo $a | cut -d "[" -f2 | cut -d "]" -f1 > listfile2
+			#cat listfile2
+			size=`stat -c %s listfile2
+			echo $size`
 		#	echo `pwd`
-			if [ ! -s listfile2 ]
+			if [ $size -gt 1 ] 
 			then
-			echo "empty file no response"
-			touch stopped-exp
-			while read r; do echo $r|grep -Po 'ExpNo="\K[^"]*'>> stopped-exp; done < /mnt/efs/training-records.txt
-                        sed -i 's/^/"/;s/$/"/' stopped-exp
-                        sed -e 's/$/            "Stopped"/' -i stopped-exp
-                        #cat stopped-exp 
-                        echo "EXPERMENT no.     Status" > outfile
-                        cat stopped-exp >> outfile
-			rm -rf sample sample2 stopped-exp 
-                        cat outfile
-
+				echo "File exists"
+				tr -s ', ' '\n' < listfile2 > running-exp
+				while read r; do grep $r /mnt/efs/training-records.txt; done < running-exp > sample
+				awk 'NR==FNR{a[$0]=1;next}!a[$0]' sample /mnt/efs/training-records.txt > sample2
+				while read r; do echo $r|grep -Po 'ExpNo="\K[^"]*'>> stopped-exp; done < sample2
+				sed -i 's/^/"/;s/$/"/' stopped-exp
+				sed -e 's/$/            "Stopped"/' -i stopped-exp
+				sed -e 's/$/            "Running"/' -i running-exp
+				cat stopped-exp >> running-exp
+				echo "EXPERMENT no.     Status" > outfile
+				cat running-exp >> outfile
+				rm -rf sample sample2 stopped-exp 
+				cat outfile
 			else 
-			tr -s ', ' '\n' < listfile2 > running-exp
-			while read r; do grep $r /mnt/efs/training-records.txt; done < running-exp > sample
-			awk 'NR==FNR{a[$0]=1;next}!a[$0]' sample /mnt/efs/training-records.txt > sample2
-			while read r; do echo $r|grep -Po 'ExpNo="\K[^"]*'>> stopped-exp; done < sample2
-			sed -i 's/^/"/;s/$/"/' stopped-exp
-			sed -e 's/$/            "Stopped"/' -i stopped-exp
-			sed -e 's/$/            "Running"/' -i running-exp
-			cat stopped-exp >> running-exp
-			echo "EXPERMENT no.     Status" > outfile
-			cat running-exp >> outfile
-			rm -rf sample sample2 stopped-exp listfile
-			cat outfile
+				echo "empty file no response"
+				touch stopped-exp
+				while read r; do echo $r|grep -Po 'ExpNo="\K[^"]*' >> stopped-exp; done < /mnt/efs/training-records.txt
+				sed -i 's/^/"/;s/$/"/' stopped-exp
+                        	sed -e 's/$/            "Stopped"/' -i stopped-exp
+				echo "EXPERMENT no.     Status" > outfile
+				cat stopped-exp >> outfile
+				rm -rf sample sample2 stopped-exp 
+                        	cat outfile
 			fi
 		fi
 	else
